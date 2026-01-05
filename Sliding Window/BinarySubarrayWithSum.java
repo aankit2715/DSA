5e85d975e2c68af1280e79dccc9f719a9fe24f29
Your Name <you@example.com>
2026-01-05 19:03:35 +0530

    sol5

---
+++ Sliding Window/BinarySubarrayWithSum.java
@@ -0,0 +1,38 @@
+// Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+// A subarray is a contiguous part of the array.
+
+// Example:
+// Input: nums = [1,0,1,0,1], goal = 2
+// Output: 4
+// Explanation: The 4 subarrays are bolded and underlined below:
+// [1,0,1,0,1]
+// [1,0,1,0,1]
+// [1,0,1,0,1]
+// [1,0,1,0,1]
+
+// ===================================================================================
+
+public class BinarySubarrayWithSum {
+
+     public int numSubarraysWithSum(int[] nums, int goal) {
+
+        int right=0, sum=0, res=0;
+        Map<Integer, Integer> prefixCount = new HashMap<>();
+        prefixCount.put(0, 1);
+
+        while(right < nums.length) {
+
+            sum += nums[right];
+
+            if(prefixCount.containsKey(sum-goal))
+                res += prefixCount.get(sum-goal);
+
+            prefixCount.put(sum, prefixCount.getOrDefault(sum, 0) +1);
+            right++;
+        }
+
+
+        return res;
+     }
+    
+}
